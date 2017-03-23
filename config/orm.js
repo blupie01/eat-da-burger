50dc790c9da764422af75c4e34e76e28607fe07d
blupie01 <jeffrey.chou.89@gmail.com>
2017-03-23 03:38:53 -0400

    server.js set up

---
+++ config/orm.js
@@ -0,0 +1,26 @@
+var connection = require('connection.js');
+
+// Question marks for sql values
+function printQuestionsMarks(num) {
+	var arr = [];
+
+	for (var i = 0; i < num; i++) {
+		arr.push("?");
+	};
+
+	return arr.toStirng();
+};
+
+// Get column names in SQL table
+function objToSql(ob) {
+	var arr = [];
+
+	for (var key in ob) {
+		if (Object.hasOwnProperty.call(ob, key)) {
+			arr.push(key + "=" + ob[key]);
+		};
+	};
+
+	return arr.toString();
+};
+
--- server.js
+++ server.js
@@ -1,3 +1,26 @@
 var express = require('express'),
 	methodOverride = require('method-override'),
 	bodyParser = require('body-parser');
+
+var port = 3000;
+
+var app = express();
+
+// Static content for the app from public directory
+app.use(express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override POST with ?_method=...
+app.use(methodOverride("_method"));
+
+// Require express-handelbars
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Set route to controller file
+var routes = require('./controllers/burgers_controller.js');
+
+app.use("/", routes);
+
+app.listen(port);
