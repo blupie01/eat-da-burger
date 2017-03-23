e6cfad3546967c18c4631805060049de0b8acb69
blupie01 <jeffrey.chou.89@gmail.com>
2017-03-23 11:49:02 -0400

    safety push

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,4 @@
-var connection = require('connection.js');
+var connection = require('./connection.js');
 
 // Question marks for sql values
 function printQuestionsMarks(num) {
@@ -24,3 +24,7 @@ function objToSql(ob) {
 	return arr.toString();
 };
 
+// ORM
+var orm = {
+	all: function()
+}
---
+++ controllers/burgers_controller.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var router = express.Router();
+
+// Import burger model
+var burger = require('../models/burger.js');
+
+// Routes
+router.get('/', function(req, res) {
+	
+})
