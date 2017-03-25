0d7d39601ad368c582e96bdff44dcab132ff287f
Jeff <Jeff>
2017-03-25 10:31:27 -0400

    repush

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require('mysql');
+var app = require('../server.js');
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: 'localhost',
-    user: 'root',
-    password: 'Avernus#1',
-    database: 'burgers_db'
-});
+if (app.settings.env == 'development') {
+    var connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: 'Avernus#1',
+        database: 'burgers_db'
+    });
+} else {
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+};
 
 connection.connect(function(err) {
     if (err) {
